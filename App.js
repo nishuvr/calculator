961d07c87a60234e45449a9ffa64e966bd1e97b5
nishuvr <51485209+nishuvr@users.noreply.github.com>
2020-05-05 17:47:32 +0530

    appearance changes

--- App.js
+++ App.js
@@ -1,66 +1,49 @@
 import React,{ Component } from 'react';
 import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
-
 export default class App extends Component {
   constructor(){
     super()
-    this.state={}
+    this.state={
+      resultText:""
+    }
   }
+  buttonpressed(text){
+    this.setState({
+      resultText:this.state.resultText+text
+    })
+              
+  }
+ 
   render(){
+    let rows=[]
+    let nums=[[1,2,3], [4,5,6], [7,8,9], [0,".","="]]
+    for(let i=0;i<4;i++){
+      let row=[]
+      for(let j=0;j<3;j++){
+        row.push(<TouchableOpacity onPress={() => this.buttonpressed(nums[i][j])} style={styles.btn}><Text style={styles.btntext}>{nums[i][j]}</Text></TouchableOpacity>)
+      }
+      rows.push(<View style={styles.row}>{row}</View>)
+    }
+    let operations=['+','-','*','/']
+    let ops=[]
+    for(let i=0;i<4;i++){
+      ops.push(<TouchableOpacity onPress={() => this.buttonpressed(operations[i])} style={styles.btn}><Text style={styles.opstext}>{operations[i]}</Text></TouchableOpacity>)
+    }
   return (
     <View style={styles.container}>
-      <View style={styles.result}></View>
-      <View style={styles.calculation}></View>
+      <View style={styles.result}>
+        <Text style={styles.resultText}>{this.state.resultText}</Text>
+      </View>
+      <View style={styles.calculation}>
+        <Text style={styles.calctext}>121</Text>
+      </View>
       <View style={styles.buttons}>
          <View style={styles.numbers}>
-             <View style={styles.row}>
-                <TouchableOpacity>
-                    <Text>1</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>2</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>3</Text>
-                </TouchableOpacity>
-             </View>
-             <View style={styles.row}>
-                <TouchableOpacity>
-                    <Text>4</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>5</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>6</Text>
-                </TouchableOpacity>
-             </View>
-             <View style={styles.row}>
-                <TouchableOpacity >
-                    <Text>7</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>8</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                <Text>9</Text>
-                </TouchableOpacity>
-             </View>
-             <View style={styles.row}>
-                <TouchableOpacity>
-                    <Text>0</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>.</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>=</Text>
-                </TouchableOpacity>
-             </View>
-
+           {rows}
          </View>
-
-         <View style={styles.operations}></View>
+         <View style={styles.operations}>
+           {ops}
+         </View>
       </View>
     </View>
   );
@@ -73,23 +56,27 @@ const styles = StyleSheet.create({
     flex: 1
     },  
     result:{
-      backgroundColor:'#ff6666',
-      flex: 2
+      backgroundColor:'#EBF5FB',
+      flex: 2,
+      justifyContent:'center',
+      alignItems:'flex-end'
     },
     buttons:{
       flexDirection:'row',
       flex:7
         },
-   calculation:{
-      backgroundColor:'#00cc99',
-      flex: 1
+    calculation:{
+      backgroundColor:'#AED6F1',
+      flex: 1,
+      justifyContent:'center',
+      alignItems:'flex-end'
     },
     numbers:{
-      backgroundColor:'#ffcc66',
+      backgroundColor:'#5DADE2',
       flex: 3
     },
     operations:{
-      backgroundColor:'black',
+      backgroundColor:'#212F3C',
       flex: 1
     },
     row:{
@@ -103,6 +90,25 @@ const styles = StyleSheet.create({
         alignItems:'center',
         alignSelf:'stretch',
         justifyContent:'center'
+    },
+    btntext:{
+      color:'#212F3C',
+      fontSize:30
+    },
+    opstext:{
+      color:'#AED6F1',
+      fontSize:30
+    },
+    resultText:{
+      color:'#5DADE2',
+      fontSize:40,
+      
+
+    },
+    calctext:
+    {
+      fontSize:40,
+      color:'#212F3C'
     }
 
    
